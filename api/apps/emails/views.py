2e629118d0c588b316cd3566fa7233740158ea7a
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-10 13:44:24 -0300

    app(api): Adiciona validação da request

--- api/apps/emails/views.py
+++ api/apps/emails/views.py
@@ -3,9 +3,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from apps.emails.serializers import EmailSerializer
+
 
 class SendEmailView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
+        serializer = EmailSerializer(data=request.data, many=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         return Response(status=status.HTTP_200_OK)
