0225439ab97f4c615dec9c86f1ec5777e947b2d6
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-10 14:23:13 -0300

    app(api): Chama o método de envio de email

--- api/apps/emails/views.py
+++ api/apps/emails/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.emails.serializers import EmailSerializer
+from apps.emails.service import EmailService
 
 
 class SendEmailView(APIView):
@@ -14,4 +15,9 @@ class SendEmailView(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(status=status.HTTP_200_OK)
+        EmailService().send_emails(serializer.validated_data)
+
+        return Response(
+            {'message': 'Todos os emails foram enviados com sucesso.'},
+            status=status.HTTP_200_OK
+        )
