56687ae2bb2b501d0b13c2c91573ad87d8cd2233
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-10 12:59:22 -0300

    app(api): Implementa método responsável por validar a existencia do token

--- api/apps/tokens/repository.py
+++ api/apps/tokens/repository.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from apps.tokens.models import Token
 from core.repository import BaseRepository
 
@@ -5,3 +7,9 @@ from core.repository import BaseRepository
 class TokenRepository(BaseRepository[Token]):
     def __init__(self) -> None:
         super().__init__(Token)
+
+    def get_by_hash(self, hash: str) -> Optional[Token]:
+        try:
+            return self.model.objects.get(hash=hash)
+        except self.model.DoesNotExist:
+            return None
