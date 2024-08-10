5dacabe10970aa12e25bba991e5c8eb5e92d23fa
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-10 13:17:51 -0300

    app(api): Atualiza o arquivo de rotas principal

--- api/core/urls.py
+++ api/core/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api/v1/admin/', admin.site.urls),
     path('api/v1/tokens/', include('apps.tokens.urls')),
+    path('api/v1/emails/', include('apps.emails.urls')),
 ]
