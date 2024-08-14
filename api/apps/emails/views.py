947a894f082a9a89b421eb0e2a2e3c83be278135
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-13 23:17:34 -0300

    app(api): Utiliza task de envio de e-mail

--- api/apps/emails/views.py
+++ api/apps/emails/views.py
@@ -1,3 +1,4 @@
+from django_q.tasks import async_task
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -5,6 +6,7 @@ from rest_framework.views import APIView
 
 from apps.emails.serializers import EmailSerializer
 from apps.emails.service import EmailService
+from apps.tasks.utils import split_list_into_sublists
 from apps.tokens.service import TokenService
 
 
@@ -23,7 +25,13 @@ class SendEmailView(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        EmailService().send_emails(serializer.validated_data)
+        print(f'Quantidade de email no pacote: {len(serializer.validated_data)}')
+
+        emails = split_list_into_sublists(serializer.validated_data, 60)
+
+        list(map(lambda p: async_task('apps.tasks.tasks.send_emails', p), emails))
+
+        # EmailService().send_emails(serializer.validated_data)
 
         return Response(
             {'message': 'Todos os emails foram enviados com sucesso.'},
